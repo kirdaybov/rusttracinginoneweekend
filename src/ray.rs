0a287397373af6b37af8906ff6adb4c6acde402c
kirdaybov <kirill.daybov@yahoo.com>
2021-09-04 23:54:01 -0400

    Split code into files, add ray struct

---
+++ src/ray.rs
@@ -0,0 +1,25 @@
+pub struct Ray {
+    orig: Vec3,
+    dir: Vec3
+}
+
+impl Ray {
+    pub fn new(origin : Vec3, direction : Vec3) -> Ray {
+        Ray {
+            orig: origin,
+            dir: direction
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t : f64) -> Vec3 {
+        self.orig + self.dir*t
+    }
+}
